d80c1a3474f6667f8738bdc1aada3a8084c4bf6f
Madison Reynolds <madison.reynolds@phoenix.edu>
2024-01-29 17:51:30 -0700

    worked on the resolvers schema

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+                return userData;
+            }
+
+            throw AuthenticationError;
+
+        },
+
+    },
+    
+    Mutation: {
+        
+    }
+}
