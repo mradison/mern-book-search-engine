e6087db7012c8eefa08232c6f3c8ecb17661541d
Madison Reynolds <madison.reynolds@phoenix.edu>
2024-01-30 08:07:45 -0700

    working on app.jsx to get the apollo and graphql working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,21 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({ uri: '/graphql', });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({ link: authLink.concat(httpLink), cache: new InMemoryCache(), });
+
 function App() {
   return (
     <ApolloProvider client={client}>
